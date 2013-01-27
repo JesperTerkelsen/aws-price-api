53c4cbb5b13c262d0e40a2cfa6c90acd4f689126
Rasmus Holmann Sørensen <rashol@infopaq.com>
2013-01-28 00:02:52 +0100

    Removed value for steps in data tiers. Not needed yet and not sure if the approach is the most fitting.

--- modules/api/src/main/java/dk/deck/aws/price/api/model/s3/DataTier.java
+++ modules/api/src/main/java/dk/deck/aws/price/api/model/s3/DataTier.java
@@ -5,21 +5,19 @@ package dk.deck.aws.price.api.model.s3;
  * @author Rasmus Holmann Sorensen
  */
 public enum DataTier {
-    FIRST_1TB("First 1 TB / month", "firstTBstorage", 1000),
-    NEXT_49TB("Next 49 TB / month", "next49TBstorage", 49000),
-    NEXT_450TB("Next 450 TB / month", "next450TBstorage", 450000),
-    NEXT_500TB("Next 500 TB / month", "next500TBstorage", 500000),
-    NEXT_4000TB("Next 4000 TB / month", "next4000TBstorage", 4000000),
-    NEXT_5000TB("Over 5000 TB / month", "over5000TBstorage", 5000000);
+    FIRST_1TB("First 1 TB / month", "firstTBstorage"),
+    NEXT_49TB("Next 49 TB / month", "next49TBstorage"),
+    NEXT_450TB("Next 450 TB / month", "next450TBstorage"),
+    NEXT_500TB("Next 500 TB / month", "next500TBstorage"),
+    NEXT_4000TB("Next 4000 TB / month", "next4000TBstorage"),
+    NEXT_5000TB("Over 5000 TB / month", "over5000TBstorage");
 
     private final String name;
     private final String id;
-    private final int sizeInGigabytes;
 
-    DataTier(String name, String id, int sizeInGigabytes){
+    DataTier(String name, String id){
         this.name = name;
         this.id = id;
-        this.sizeInGigabytes = sizeInGigabytes;
     }
 
     public String getName(){
@@ -28,10 +26,6 @@ public enum DataTier {
 
     public String getId(){
         return id;
-    }
-
-    public int getSizeInGigabytes() {
-        return sizeInGigabytes;
     }
 
     public static DataTier getTierById(String id){
